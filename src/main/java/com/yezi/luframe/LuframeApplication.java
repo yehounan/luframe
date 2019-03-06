f44eb16679dc985e72ed12800385512a97c06d40
yehounan <yehn032@udduo.com>
2019-03-06 14:51:46 +0800

    注释

--- src/main/java/com/yezi/luframe/LuframeApplication.java
+++ src/main/java/com/yezi/luframe/LuframeApplication.java
@@ -3,11 +3,15 @@ package com.yezi.luframe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yezi
+ * @date 2019/2/25 10:43
+ */
 @SpringBootApplication
 public class LuframeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LuframeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LuframeApplication.class, args);
+    }
 
 }
