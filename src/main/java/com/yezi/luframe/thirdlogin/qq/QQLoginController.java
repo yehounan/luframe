0c1511d113cca3245b8a1bcb8ae8777ca399fcf9
yehounan <yehn032@udduo.com>
2019-03-06 17:45:37 +0800

    微信第三方登录

---
+++ src/main/java/com/yezi/luframe/thirdlogin/qq/QQLoginController.java
@@ -0,0 +1,11 @@
+package com.yezi.luframe.thirdlogin.qq;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yezi
+ * @date 2019/3/6 17:45
+ */
+@RestController
+public class QQLoginController {
+}
